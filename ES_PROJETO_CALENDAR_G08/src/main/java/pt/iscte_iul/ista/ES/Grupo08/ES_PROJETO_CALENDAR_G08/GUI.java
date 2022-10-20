f13cc437d9804bfee7596775260bcb7d75b57f9c
timso-iscte <timso@iscte-iul.pt>
2022-10-20 15:11:47 +0100

    Created Java Class where GUI will be implemented. Start of the initial Jframe

---
+++ ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/GUI.java
@@ -0,0 +1,16 @@
+package pt.iscte_iul.ista.ES.Grupo08.ES_PROJETO_CALENDAR_G08;
+
+import java.awt.*;  
+import java.awt.event.*;  
+import javax.swing.*;
+
+public class GUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	JFrame initial_frame = new JFrame();
+
+}
