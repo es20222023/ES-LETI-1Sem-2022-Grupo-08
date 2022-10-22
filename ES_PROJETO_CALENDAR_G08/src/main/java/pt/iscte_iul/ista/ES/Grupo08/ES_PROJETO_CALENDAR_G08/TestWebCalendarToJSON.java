11dec6d1f154cce344af5a56b5103c4e92f5ffbd
Pedro Luis <pmaal1@iscte-iul.pt>
2022-10-22 19:31:38 +0100

    Updating branch

--- ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/TestWebCalendarToJSON.java
+++ ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/TestWebCalendarToJSON.java
@@ -2,11 +2,20 @@ package pt.iscte_iul.ista.ES.Grupo08.ES_PROJETO_CALENDAR_G08;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.URL;
+
 import java.util.regex.Pattern;
+
+
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
+
 
 /**
  * Hello world!
@@ -18,18 +27,37 @@ public class TestWebCalendarToJSON
     	
     	String URL = readUrl("https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=pmaal1@iscte.pt&password=4nW90X1wHzGP2YQc5ardt24MEz9hEACP0uss6KwnUXgO76bZcF2NLXjzdmqaF738FVbA9Uhu3ADP5pAMVBkftzHfDvzoMBMe5jPdWVRboFdCpfW02WbnAnSN6eWkeGd7");
     	//System.out.println(URL);
+    	
+    	//System.out.println(URL.toString());
     	String[] strArray = StrtoStrArray(URL);
-    	for (int i = 0; i< strArray.length; i++){  
+    	/*for (int i = 0; i< strArray.length; i++){  
     		System.out.println(strArray[i]); 
-    	}
+    	}*/
     	String jsonArray = jSonArray(strArray);
-    	System.out.println(jsonArray);
+    	Component cal = getCalendarEvent(URL); 
+    	System.out.println(cal.toString());
+    	
     	
     	
     	
     	
    
 }
+    public static Component getCalendarEvent(String myCalendarString) 
+    {
+        try {
+            StringReader sin = new StringReader(myCalendarString);
+            CalendarBuilder builder = new CalendarBuilder();
+            Calendar calendar = builder.build(sin);
+
+            return (Component)calendar.getComponents("VEVENT");
+
+        } catch (Exception e) {e.printStackTrace();}
+
+        return null;
+    }
+    
+    
     
     private static String jSonArray(String[] strArray) {
     	Gson gson=new GsonBuilder().create();
