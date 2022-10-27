5cefadd49605e12572ee07a08c42ba380d07b1db
Pedro-Iscte <pmaal1@iscte-iul.pt>
2022-10-27 14:54:38 +0100

    Dependency and ClassPath update
    Replaced "jackson-core".jar with jackson-core maven dependency

--- ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/TestWebCalendarToJSON.java
+++ ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/TestWebCalendarToJSON.java
@@ -1,25 +1,16 @@
 package pt.iscte_iul.ista.ES.Grupo08.ES_PROJETO_CALENDAR_G08;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Paths;
+
 import java.util.Scanner;
-import java.util.regex.Pattern;
+
+
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import net.fortuna.ical4j.data.CalendarBuilder;
-import net.fortuna.ical4j.model.Calendar;
-import net.fortuna.ical4j.model.Component;
 
 import org.json.simple.*;
 import org.apache.commons.lang3.StringUtils;
