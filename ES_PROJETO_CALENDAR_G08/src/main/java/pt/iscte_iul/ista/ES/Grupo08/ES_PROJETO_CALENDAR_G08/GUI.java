e7636e5bc765ef0b80a755749008d2e27bfd1803
timso-iscte <timso@iscte-iul.pt>
2022-10-30 17:44:59 +0000

    Added usernaem input

--- ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/GUI.java
+++ ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/GUI.java
@@ -6,44 +6,44 @@ import javax.swing.*;
 
 public class GUI {
 
-	static String var;
-	public static void main(String[] args) {
+	static String names = "";
+	private static JLabel calendar_names = new JLabel();
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		initial_frame();
 	}
-	
-	private static void initial_frame() {
+	private static void initial_frame() throws Exception {
 	JFrame initial_frame = new JFrame("First Frame");
-	JButton b=new JButton("Click Here");  
-    b.setBounds(50,100,95,30); 
-    final JTextField input1 = new JTextField("Paste your URI here");
-	input1.setBounds(50,50, 200, 45);
-    initial_frame.setSize(400,400);  
+	JButton b=new JButton("Add Calendar"); 
+	//private JLabel calendar_names = new JLabel();
+	calendar_names.setText("test");
+	calendar_names.setBounds(500, 50, 100, 45);
+    b.setBounds(50, 100, 150, 30); 
+    final JTextField input_name = new JTextField("username");
+    final JTextField input_URI = new JTextField("Paste your URI here");
+	input_name.setBounds(50, 50, 100, 45);
+	input_URI.setBounds(151, 50, 200, 45);
+    initial_frame.setSize(800, 800);  
     initial_frame.setLayout(null);  
     initial_frame.setVisible(true);   
     initial_frame.add(b);
-    initial_frame.add(input1);
-    input1.addActionListener(new ActionListener() { // Action to be performed when "Enter" key is pressed
+    initial_frame.add(input_name);
+    initial_frame.add(input_URI);
+    initial_frame.add(calendar_names);
+    b.addActionListener(new ActionListener() { // Action to be performed when "Enter" key is pressed
         @Override
         public void actionPerformed(ActionEvent e) {
-            var = input1.getText(); // Getting text input from JTextField(tf)
-            System.out.println(var);
+        	names = names + (input_name.getText()+"</br>");
+            calendar_names.setText(names);
+            
+            
+            
+
         }
     });
     
-	}
+    }
 	
 	
-//	private static Component uri_input() {
-//		
-//		JTextField input1 = new JTextField("Paste your URI here");
-//		input1.setBounds(50,50, 200, 45);
-//		return input1;		
-//		
-//		input1.addActionListener(new ActionListener() { // Action to be performed when "Enter" key is pressed
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                var = input1.getText(); 
-//            }
-//	}
+	
 }
