cda87f1198a08a52e4a650755d56c1f0505cac8a
timso-iscte <timso@iscte-iul.pt>
2022-11-17 11:17:42 +0000

    Added App as main

---
+++ ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/App.java
@@ -0,0 +1,13 @@
+package pt.iscte_iul.ista.ES.Grupo08.ES_PROJETO_CALENDAR_G08;
+
+
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		WeekCalendar cal = new WeekCalendar();
+
+	}
+
+}
