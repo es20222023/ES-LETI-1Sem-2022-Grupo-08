9b3979625d010c7e3ee4bc2119735b7c081cfec3
timso-iscte <timso@iscte-iul.pt>
2022-11-17 15:34:34 +0000

    Started implementing the main class to draw graphics. Successful.

--- ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/App.java
+++ ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/App.java
@@ -1,18 +1,57 @@
 package pt.iscte_iul.ista.ES.Grupo08.ES_PROJETO_CALENDAR_G08;
 
-import java.util.ArrayList;
-import java.time.*;
 import com.davidmoodie.SwingCalendar.*;
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class App {
 
 	public static void main(String[] args) {
 		
+		JFrame frm = new JFrame();
+		
 		ArrayList<CalendarEvent> events = new ArrayList<>();
 		events.add(new CalendarEvent(LocalDate.of(2022, 11, 11), LocalTime.of(14, 0), LocalTime.of(14, 20), "Test 11/11 14:00-14:20"));
 		
-		Calendar cal = new WeekCalendar(events);
+		WeekCalendar cal = new WeekCalendar(events);
+		
+		cal.addCalendarEventClickListener(e -> System.out.println(e.getCalendarEvent()));
+        cal.addCalendarEmptyClickListener(e -> {
+            System.out.println(e.getDateTime());
+            System.out.println(Calendar.roundTime(e.getDateTime().toLocalTime(), 30));
+        });
 
+        JButton goToTodayBtn = new JButton("Today");
+        goToTodayBtn.addActionListener(e -> cal.goToToday());
+
+        JButton nextWeekBtn = new JButton(">");
+        nextWeekBtn.addActionListener(e -> cal.nextWeek());
+
+        JButton prevWeekBtn = new JButton("<");
+        prevWeekBtn.addActionListener(e -> cal.prevWeek());
+
+        JButton nextMonthBtn = new JButton(">>");
+        nextMonthBtn.addActionListener(e -> cal.nextMonth());
+
+        JButton prevMonthBtn = new JButton("<<");
+        prevMonthBtn.addActionListener(e -> cal.prevMonth());
+
+        JPanel weekControls = new JPanel();
+        weekControls.add(prevMonthBtn);
+        weekControls.add(prevWeekBtn);
+        weekControls.add(goToTodayBtn);
+        weekControls.add(nextWeekBtn);
+        weekControls.add(nextMonthBtn);
+
+        frm.add(weekControls, BorderLayout.NORTH);
+
+        frm.add(cal, BorderLayout.CENTER);
+        frm.setSize(1000, 900);
+        frm.setVisible(true);
+        frm.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 
 }
