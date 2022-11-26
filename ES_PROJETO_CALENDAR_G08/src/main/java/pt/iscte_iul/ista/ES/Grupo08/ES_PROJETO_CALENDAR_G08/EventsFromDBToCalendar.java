bbe5886035a4a918a8f1406d3885d0880951e4ec
Pedro-Iscte <pmaal1@iscte-iul.pt>
2022-11-26 17:43:06 +0000

    Create EventsFromDBToCalendar class

---
+++ ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/EventsFromDBToCalendar.java
@@ -0,0 +1,24 @@
+package pt.iscte_iul.ista.ES.Grupo08.ES_PROJETO_CALENDAR_G08;
+
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+
+import com.davidmoodie.SwingCalendar.CalendarEvent;
+
+public class EventsFromDBToCalendar {
+	
+	
+	public ArrayList<CalendarEvent> JSonObjectToCalendarEvent(JSONArray events){
+		
+		ArrayList<CalendarEvent> calendarEventList = new ArrayList<>();
+		for(int i = 0; i < events.size(); i++) {
+			
+			
+		}
+		
+		
+		return null;
+	}
+
+}
