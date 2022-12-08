aa7a7bd9cf59af9893f27586f3d32e0ed7a48805
Pedro Luis <pmaal1@iscte-iul.pt>
2022-12-08 17:35:11 +0000

    Generate Javadoc for DB_Operations class

--- ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/DB_Operations.java
+++ ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/DB_Operations.java
@@ -23,13 +23,27 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
+/**
+ * Class with the MongoDB database operations.
+ * @author Pedro Luis
+ * @version 08/12/2022
+ *
+ */
 public class DB_Operations {
 
 	String username;
+
+	/**
+	 * Constructor
+	 */
 	public DB_Operations() {
 		username = "";
 	}
 
+	/**
+	 * Main method for small tests.
+	 * @param args Main method arguments.
+	 */
 	public static void main(String[] args) {
 		DB_Operations entry = new DB_Operations();
 		entry.icsToDB("webcal://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=pmaal1@iscte.pt&password=4nW90X1wHzGP2YQc5ardt24MEz9hEACP0uss6KwnUXgO76bZcF2NLXjzdmqaF738FVbA9Uhu3ADP5pAMVBkftzHfDvzoMBMe5jPdWVRboFdCpfW02WbnAnSN6eWkeGd7");
@@ -40,6 +54,10 @@ public class DB_Operations {
 
 	}
 
+	/**
+	 * Method to send Calendar info directly from Fenix URL into MongoDB database.
+	 * @param url_str String with the Fenix calendar URL 
+	 */
 	@SuppressWarnings("unchecked")
 	public void icsToDB(String url_str) {
 		try {
@@ -99,6 +117,12 @@ public class DB_Operations {
 	}
 
 
+	/**
+	 * Method to get the Calendar info from MongoDB given an username.
+	 * @param username Username to access the right database collection.
+	 * @return JSONArray with JSON objects that will be used to generate an Array with CalendarEvents in EventsFromDBToCalendar class.
+	 */
+	
 	@SuppressWarnings("unchecked")
 	public JSONArray readFromDB(String username) {
 		JSONArray calendar = new JSONArray();
@@ -135,6 +159,9 @@ public class DB_Operations {
 
 	}
 
+	/**
+	 * Method to completely clear the Project database.
+	 */
 	public void clearDB() {
 		try {
 			ConnectionString connectionString = new ConnectionString("mongodb+srv://projetoesgrupo08:ProjetoESGrupo08@cluster0.qgzbwsz.mongodb.net/?retryWrites=true&w=majority");
@@ -150,14 +177,28 @@ public class DB_Operations {
 		}
 	}
 	
+	/**
+	 * Method to ensure that there is no duplicate data in each collection of the project database.
+	 * @param coll
+	 */
 	private void dropCollection(MongoCollection<Document> coll) {
 		coll.drop();
 	}
 
+	/**
+	 * Replace webcal protocol from the Fenix URL with https protocol.
+	 * @param str
+	 * @return
+	 */
 	private String replace(String str) {
 		return str.replace("webcal", "https");
 	}
 
+	/**
+	 * Get username directly from the Fenix URL.
+	 * @param str
+	 * @return
+	 */
 	private String getUsername(String str) {
 		return StringUtils.substringBetween(str, "username=", "@");
 	}
