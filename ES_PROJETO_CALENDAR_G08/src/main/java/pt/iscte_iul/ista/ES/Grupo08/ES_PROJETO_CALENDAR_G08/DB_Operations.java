2172b505438920bcbd2245237b54977303e743ff
Pedro Luis <pmaal1@iscte-iul.pt>
2022-12-08 16:48:45 +0000

    Ensure that there is no duplicate data in DB. Added clearDB() method.

--- ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/DB_Operations.java
+++ ES_PROJETO_CALENDAR_G08/src/main/java/pt/iscte_iul/ista/ES/Grupo08/ES_PROJETO_CALENDAR_G08/DB_Operations.java
@@ -3,6 +3,7 @@ package pt.iscte_iul.ista.ES.Grupo08.ES_PROJETO_CALENDAR_G08;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.apache.commons.lang3.StringUtils;
@@ -28,38 +29,46 @@ public class DB_Operations {
 	public DB_Operations() {
 		username = "";
 	}
-	
+
 	public static void main(String[] args) {
 		DB_Operations entry = new DB_Operations();
 		entry.icsToDB("webcal://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=pmaal1@iscte.pt&password=4nW90X1wHzGP2YQc5ardt24MEz9hEACP0uss6KwnUXgO76bZcF2NLXjzdmqaF738FVbA9Uhu3ADP5pAMVBkftzHfDvzoMBMe5jPdWVRboFdCpfW02WbnAnSN6eWkeGd7");
 		JSONArray output = entry.readFromDB(entry.username);
 		System.out.println(output);
 		System.out.println(output.size());
-		
+		//entry.clearDB();
+
 	}
 
+	@SuppressWarnings("unchecked")
 	public void icsToDB(String url_str) {
 		try {
 			URL url = new URL(replace(url_str));
 			username = getUsername(url_str);
-			
+
 			Scanner s = new Scanner(url.openStream());
 
 			try {
 				ConnectionString connectionString = new ConnectionString("mongodb+srv://projetoesgrupo08:ProjetoESGrupo08@cluster0.qgzbwsz.mongodb.net/?retryWrites=true&w=majority");
 				MongoClientSettings settings = MongoClientSettings.builder()
-				        .applyConnectionString(connectionString)
-				        .build();
+						.applyConnectionString(connectionString)
+						.build();
 				MongoClient mongoClient = MongoClients.create(settings);
 				MongoDatabase database = mongoClient.getDatabase("ProjetoES");
-				MongoCollection<Document> coll = database.getCollection("Eventos_"+username);
+				String collectionName = "Eventos_"+username;
+				MongoCollection<Document> coll = database.getCollection(collectionName);
 				
-			
+				@SuppressWarnings("rawtypes")
+				boolean collectionExists = database.listCollectionNames()
+						  .into(new ArrayList()).contains(collectionName);
+				if (collectionExists)
+					dropCollection(coll);
+
 				String line = s.nextLine();
 				while (s.hasNext()) {
 					if(line.contains("DTSTART")) {
 						Document doc = new Document();
-					
+
 						doc.put("START", StringUtils.substringAfter(line, ":"));
 						line = s.nextLine();
 						doc.put("END", StringUtils.substringAfter(line, ":"));
@@ -67,17 +76,17 @@ public class DB_Operations {
 						doc.put("SUMMARY", StringUtils.substringAfter(line, ":"));
 
 						//Creates an event object that contains de json object that contains the details
-						
+
 						coll.insertOne(doc);
 
-						
+
 						line = s.nextLine();  
 
 					} else line = s.nextLine();
 
 				}
-				
-	
+
+
 			} 
 			catch (Exception e) {
 				e.printStackTrace();
@@ -89,49 +98,68 @@ public class DB_Operations {
 		}
 	}
 
-	
+
 	@SuppressWarnings("unchecked")
 	public JSONArray readFromDB(String username) {
 		JSONArray calendar = new JSONArray();
 		JSONParser parser = new JSONParser();  
-		 
+
 		try {
 			ConnectionString connectionString = new ConnectionString("mongodb+srv://projetoesgrupo08:ProjetoESGrupo08@cluster0.qgzbwsz.mongodb.net/?retryWrites=true&w=majority");
 			MongoClientSettings settings = MongoClientSettings.builder()
-			        .applyConnectionString(connectionString)
-			        .build();
+					.applyConnectionString(connectionString)
+					.build();
 			MongoClient mongoClient = MongoClients.create(settings);
 			MongoDatabase database = mongoClient.getDatabase("ProjetoES");
 			MongoCollection<Document> coll = database.getCollection("Eventos_"+username);
-			
+
 			MongoCursor<Document> cursor = coll.find().iterator();
-			
+
 			try {
 				while(cursor.hasNext()) {
 					String event = cursor.next().toJson();
 					JSONObject json = (JSONObject) parser.parse(event);
 					calendar.add(json);
-					
+
 				}
 			} finally {
 				cursor.close();
 			}
-			
-	
+
+
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 		return calendar;
-		
+
+	}
+
+	public void clearDB() {
+		try {
+			ConnectionString connectionString = new ConnectionString("mongodb+srv://projetoesgrupo08:ProjetoESGrupo08@cluster0.qgzbwsz.mongodb.net/?retryWrites=true&w=majority");
+			MongoClientSettings settings = MongoClientSettings.builder()
+					.applyConnectionString(connectionString)
+					.build();
+			MongoClient mongoClient = MongoClients.create(settings);
+			MongoDatabase database = mongoClient.getDatabase("ProjetoES");
+			database.drop();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
+	private void dropCollection(MongoCollection<Document> coll) {
+		coll.drop();
+	}
+
 	private String replace(String str) {
 		return str.replace("webcal", "https");
 	}
-	
+
 	private String getUsername(String str) {
 		return StringUtils.substringBetween(str, "username=", "@");
 	}
-	
+
 }
