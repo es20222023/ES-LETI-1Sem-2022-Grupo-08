88985c6ee26e5e2f29954fa9cf7fe392e9278392
Pedro Luis <pmaal1@iscte-iul.pt>
2022-12-08 17:49:26 +0000

    Changed Javadoc.

--- ES_PROJETO_CALENDAR_G08/doc/type-search-index.js
+++ ES_PROJETO_CALENDAR_G08/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"pt.iscte_iul.ista.ES.Grupo08.ES_PROJETO_CALENDAR_G08","l":"DB_Operations"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"pt.iscte_iul.ista.ES.Grupo08.ES_PROJETO_CALENDAR_G08","l":"App"}];updateSearchResults();
